d3067dc28c985c0b5fe9905eaeec87c6c7e540ed
dineshchandr <dinusai05@gmail.com>
2025-10-16 18:02:49 +0530

    Initial commit: add user service feature skeleton

---
+++ src/test/java/net/ecommerce/microservices/EcommerceMicroservicesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ecommerce.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMicroservicesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
