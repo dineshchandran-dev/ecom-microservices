c2c07ec532da060c4e93f9aa8c0f3423aa7bc6b1
dineshchandr <dinusai05@gmail.com>
2025-10-22 18:41:50 +0530

    Update ResponseEntity

--- src/main/java/net/ecommerce/microservices/user/controller/UserController.java
+++ src/main/java/net/ecommerce/microservices/user/controller/UserController.java
@@ -4,14 +4,16 @@ import lombok.RequiredArgsConstructor;
 import net.ecommerce.microservices.user.entity.User;
 import net.ecommerce.microservices.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,15 +23,28 @@ public class UserController {
    private UserService userService;
 
     @GetMapping("/user")
-    public List<User> fetchAllUser(){
-        return userService.viewAllUser();
+    public ResponseEntity<List<User>> fetchAllUser() {
+        return new ResponseEntity<>(userService.viewAllUser(), HttpStatus.OK);
     }
 
 
     @PostMapping("/user")
-    public List<User> AddUser(@RequestBody User user){
+    public ResponseEntity<List<User>> AddUser(@RequestBody User user) {
 
-       return  userService.addUser(user);
+        return new ResponseEntity<>(userService.addUser(user), HttpStatus.OK);
+    }
+
+    @GetMapping("/user/retrieve/{id}")
+    public ResponseEntity<User> retrieve(@PathVariable Long id) {
+
+//        Optional<User> user= userService.retrieveUser(id);
+//        if(user==null){
+//            return  ResponseEntity.notFound().build();
+//        }
+//        return new ResponseEntity<>(user,HttpStatus.OK);
+
+        return userService.retrieveUser(id).map(ResponseEntity::ok).orElseGet(()->ResponseEntity.notFound().build());
+
     }
 
 }
--- src/main/java/net/ecommerce/microservices/user/service/UserService.java
+++ src/main/java/net/ecommerce/microservices/user/service/UserService.java
@@ -8,10 +8,11 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
-
+  private  Long num=1L;
 
     private final List<User>userList=new ArrayList<>();
 
@@ -20,8 +21,15 @@ public class UserService {
     }
 
     public List<User> addUser(User user){
+        user.setId(num++);
         userList.add(user);
         return  userList;
     }
+
+    public Optional<User> retrieveUser(Long id){
+
+  return userList.stream().filter(user-> user.getId().equals(id)).findFirst();
+    }
+
 
 }
