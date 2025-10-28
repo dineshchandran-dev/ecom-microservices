766cac5a5d9759aad43c5809a2d5e1314be86713
dineshchandr <dinusai05@gmail.com>
2025-10-28 17:55:02 +0530

    user

--- src/main/java/net/ecommerce/microservices/user/controller/UserController.java
+++ src/main/java/net/ecommerce/microservices/user/controller/UserController.java
@@ -1,41 +1,42 @@
 package net.ecommerce.microservices.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import net.ecommerce.microservices.user.dto.RequestDto;
+import net.ecommerce.microservices.user.dto.UserResponseDto;
 import net.ecommerce.microservices.user.entity.User;
 import net.ecommerce.microservices.user.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
 
-   @Autowired
-   private UserService userService;
+    private final UserService userService;
 
-    @GetMapping("/user")
-    public ResponseEntity<List<User>> fetchAllUser() {
+    @GetMapping
+    public ResponseEntity<List<UserResponseDto>> fetchAllUser() {
         return new ResponseEntity<>(userService.viewAllUser(), HttpStatus.OK);
     }
 
 
-    @PostMapping("/user")
-    public ResponseEntity<List<User>> AddUser(@RequestBody User user) {
+    @PostMapping
+    public ResponseEntity<List<UserResponseDto>> AddUser(@RequestBody User user) {
 
         return new ResponseEntity<>(userService.addUser(user), HttpStatus.OK);
     }
 
-    @GetMapping("/user/retrieve/{id}")
-    public ResponseEntity<User> retrieve(@PathVariable Long id) {
+    @GetMapping("/retrieve/{id}")
+    public ResponseEntity<UserResponseDto> retrieve(@PathVariable Long id) {
 
 //        Optional<User> user= userService.retrieveUser(id);
 //        if(user==null){
@@ -45,6 +46,10 @@ public class UserController {
 
         return userService.retrieveUser(id).map(ResponseEntity::ok).orElseGet(()->ResponseEntity.notFound().build());
 
+    }
+    @PostMapping("/update/{id}")
+    public ResponseEntity<UserResponseDto> update(@PathVariable Long id, @RequestBody RequestDto user) {
+        return new ResponseEntity<>(userService.updateUserDetails(id,user),HttpStatus.OK);
     }
 
 }
